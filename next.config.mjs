c1451e2cae9c1c6852702fd131e2793c4e972557
Albert Charmelo <albertcharmelocontacto@gmail.com>
2024-04-11 14:11:27 -0400

    Añadir configuración de Prettier y ESLint, y actualizar dependencias

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['cdn.sanity.io', 'raw.githubusercontent.com'],
+  },
+};
 
 export default nextConfig;
